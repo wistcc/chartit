43c926f14f53a2d851cacffb9171d1a1d09b1e6b
Winner Crespo <w.crespo-cruz@klarna.com>
2019-09-26 22:32:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="sheetUrl">
+    <button @click="convertSheetToJson">
+      Convert to JSON
+    </button>
+    
+    <ChartLine
+      v-if="line"
+      :config="line" />
+  </div>
+</template>
+
+<script>
+import { getJsonObject } from 'sheetyjs'
+import { ChartLine } from 'chart.xkcd-vue-wrapper';
+
+export default {
+  name: 'app',
+  components: {
+    ChartLine
+  },
+  data: function () {
+    return {
+      sheetUrl: '',
+      line: null,
+    }
+  },
+  methods:{
+    async convertSheetToJson() {
+      const values = await getJsonObject(this.sheetUrl)
+      const data = []
+
+      values.forEach(value => {
+        Object.keys(value).forEach((key, index) => {
+          if (!data[index]) {
+            data[index] = []
+          }
+
+          if (!data[index].length) {
+            data[index].push(key)
+          }
+          data[index].push(value[key])
+        });
+      });
+
+      const labels = data.splice(0, 1)[0]
+      const datasets = data.reduce((prev, curr) => {
+        prev.push({
+          label: curr.splice(0, 1)[0],
+          data: curr,
+        })
+        return prev
+      }, [])
+      
+      this.line = {
+        title: '30 days pushups challenge',
+        xLabel: labels.splice(0, 1)[0],
+        // yLabel: 'Pushups',
+        data: {
+          labels: labels,
+          datasets,
+        },
+      }
+    }
+  },
+}
+</script>
+
+<style>
+</style>
